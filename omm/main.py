9b7031ba043bb6706e2911029a2af517912d12f0
Mark S. Brown <mbrown@ee.ucl.ac.uk>
2014-07-31 12:55:25 +0100

    added excluded directories

--- omm/main.py
+++ omm/main.py
@@ -18,18 +18,26 @@ from __future__ import print_function, division
 from collections import Iterable
 import os
 
-def recursive_data_search(key_words, allowed_ext=('txt', 'brdf'), verbose=0):
+def recursive_data_search(key_words, allowed_ext=('txt', 'brdf'), ignored_directories=('optical_measurement_module', 'archive', '/.'), verbose=0):
     """
     Traversal over directory to yield full location of matching files
     """
-    
-    
+
     if isinstance(key_words, Iterable) and isinstance(key_words, str):
         key_words = [key_words, ]
 
     for key_word in key_words:
         for root, directories, all_files in os.walk(os.getcwd()):
+
+            if any(root.find(a_directory) >= 0 for a_directory in ignored_directories):
+                if verbose > 0:
+                    print("Skipping", root)
+                continue
+
             for a_file in all_files:
+
+                if verbose > 0:
+                    print("Root : {} \nFile: {}".format(root, a_file))
 
                 if not any(a_file.endswith(ext) for ext in allowed_ext) or not a_file.startswith(key_word):
                     continue
